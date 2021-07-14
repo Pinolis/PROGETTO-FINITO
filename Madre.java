23a4213c742cff4793759c3168f3b5c61198bfa3
BboyCaligola <84084942+BboyCaligola@users.noreply.github.com>
2021-07-14 23:33:11 +0200

    Create Madre.java

---
+++ Madre.java
@@ -0,0 +1,20 @@
+
+/**the Madre class represents the wife of the current emperor
+ * and the mother of the future emperor
+ * 
+ * @author tobia
+ */
+public class Madre extends Persona {
+
+    /**create the instance taking the name
+     * and the wikipedia's page url of the
+     * mother we want to represent
+     * 
+     * @param nome name of the person
+     * @param url url wikipedia page
+     */
+	public Madre(String nome, String url) {
+		super(nome, url);
+	}
+
+}
