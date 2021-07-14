2e48cd046c1c0a2bd82373f551ffd4b8ed7f67e5
BboyCaligola <84084942+BboyCaligola@users.noreply.github.com>
2021-07-14 23:31:27 +0200

    Create ScraperSeveri.java

---
+++ ScraperSeveri.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class ScraperSeveri extends Scraper{
+
+	public ScraperSeveri(Dinastia dinastia, WebDriver driver) {
+		super(dinastia, driver);
+	}
+	public void setBackbone() {
+		super.setBackbonePart1();
+		// Nel caso dei Severi
+		ArrayList<Imperatore> backbone = getBackbone();
+		// Sostituisco Macrino e Diadumeniano con Eliogabalo e Alessandro Severo
+		backbone.remove(4); //Ho invertito gli indici da eliminare perché altrimenti cancellavamo Eliogabalo
+		backbone.remove(3);
+		super.setBackbonePart2();
+	}
+}
