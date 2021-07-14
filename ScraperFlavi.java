d14479563b3327aa84e7947fc6b1cf141a13bc84
BboyCaligola <84084942+BboyCaligola@users.noreply.github.com>
2021-07-14 23:31:57 +0200

    Create ScraperFlavi.java

---
+++ ScraperFlavi.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebDriver;
+
+/**extends Scraper, manages the case in which the dinasty is Flavia
+ * 
+ * @author tobia
+ *
+ */
+public class ScraperFlavi extends Scraper{
+	
+    /**takes a dinasty and a driver,
+     * builds the instance
+     * 
+     * @param dinastia the dinasty on which you want to work
+     * @param driver the driver you want to use
+     */
+	public ScraperFlavi(Dinastia dinastia, WebDriver driver) {
+		super(dinastia, driver);
+	}
+	
+	/**set the wives, sons, parents and other info
+	 * for the input imperatore
+	 * 
+	 * @param imperatore the emperor you want to work on
+	 */
+	public void setRelatives(Imperatore imperatore) {
+		super.setRelatives(imperatore);
+		// tolgo dalle mogli alcuni link errati
+		ArrayList<Persona> mogli = imperatore.getMogli();
+		for (int n=mogli.size()-1; n>=0;n--) {
+			if (anyNumber(mogli.get(n).getNome())) {
+				mogli.remove(n);
+			}
+		}
+		
+	}
+}
