fbbed1609d181d4954e619a427edbffc1906a82c
Carcosa777 <84084943+Carcosa777@users.noreply.github.com>
2021-07-17 23:40:11 +0200

    Update Azione2.java

--- Azione2.java
+++ Azione2.java
@@ -1,17 +1,31 @@
 package scrap;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+/**
+ * This ActionListener will reset the frame allowing the user to choose a 
+ * new dinasty to be dispayed
+ * 
+ * @author Andrea, Edoardo, Federico
+ *
+ */
+
 public class Azione2 implements ActionListener {
 	private JFrame frame;
 	
+	/**
+	 * Constructor of the method
+	 * 
+	 * @param f the frame
+	 */
 	public Azione2(JFrame f) {
 		frame=f;
 	}
 	
-	
+
 	public void actionPerformed(ActionEvent event) {
 		frame.dispose();
 		new Frame();
